002ee970f4d120c1e176ed3ea4a3c5eec9b99296
aleksabanjanin1158 <153463533+aleksabanjanin1158@users.noreply.github.com>
2024-04-16 22:45:06 -0400

    added UI

--- browser-history-manager.c
+++ browser-history-manager.c
@@ -5,16 +5,16 @@
 #include <string.h>
 #pragma warning(disable: 4996)
 
-struct Node 
+struct Node
 {
     char month[20];
     float expenses;
     struct Node* next;
     struct Node* prev;
 };
-struct Node* createExpense(char month,float expenses){
+struct Node* createExpense(char month, float expenses) {
     struct Node* newExpense = (struct Node*)malloc(sizeof(struct Node));
-    if(Node == NULL){
+    if (Node == NULL) {
         printf("No Memory!");
         exit(EXIT_FAILURE);
     }
@@ -24,9 +24,9 @@ struct Node* createExpense(char month,float expenses){
     Node->prev = NULL;
     return newExpense;
 }
-struct Node* InsertNewExpense(struct Node* expenses,struct Node** lastExpense,char* month,float expenese){
-    struct Node* newExpense = CreateExpense(month,expense);
-    if(expnse == NULL ){
+struct Node* InsertNewExpense(struct Node* expenses, struct Node** lastExpense, char* month, float expenese) {
+    struct Node* newExpense = CreateExpense(month, expense);
+    if (expnse == NULL) {
         *lastExpenses = newExpense;
         return newExpnse;
     }
@@ -34,9 +34,9 @@ struct Node* InsertNewExpense(struct Node* expenses,struct Node** lastExpense,ch
     expenses->prev = newExpense;
     return expenses;
 }
-struct Node* InsertAtEnd(struct Node* expenses,struct Node** lastExpense,char* month,float expenese){
-    struct Node* newExpense = CreateExpense(month,expense);
-    if(lastExpense == NULL){
+struct Node* InsertAtEnd(struct Node* expenses, struct Node** lastExpense, char* month, float expenese) {
+    struct Node* newExpense = CreateExpense(month, expense);
+    if (lastExpense == NULL) {
         printf("No Memory");
         exit(EXIT_FAILURE);
     }
@@ -45,51 +45,86 @@ struct Node* InsertAtEnd(struct Node* expenses,struct Node** lastExpense,char* m
     newExpense->next = NULL;
 }
 
-float searchMonthExpenses(struct Node* head, char* target_month) 
+float searchMonthExpenses(struct Node* head, char* target_month)
 {
     struct Node* current = head;
 
-    
+
     while (current != NULL)
     {
-        
-        if (strcmp(current->month, target_month) == 0) 
+
+        if (strcmp(current->month, target_month) == 0)
         {
-            
+
             return current->expenses;
         }
-       
+
         current = current->next;
     }
 
-   
-    return -1; 
+
+    return -1;
 }
 
-int main(void) 
+void displayMainMenu() {
+    printf("\n-- Main Menu --\n");
+    printf("1. View Overall Expenses\n");
+    printf("2. View Monthly Expenses\n");
+    printf("3. Add New Expense\n");
+    printf("4. Delete Expense\n");
+    printf("5. Exit\n");
+    printf("Select an option: ");
+}
+
+void displayOverallExpenses(struct Node* head) {
+    float totalExpenses = 0;
+    struct Node* current = head;
+    printf("\n-- Overall Expenses --\n");
+    while (current != NULL) {
+        totalExpenses += current->expenses;
+        current = current->next;
+    }
+    printf("Total Expenses: $%.2f\n", totalExpenses);
+}
+
+void displayMonthlyExpenses(struct Node* head) {
+    char month[20];
+    printf("\nEnter the month to view expenses: ");
+    scanf("%s", month);
+    float expenses = searchMonthExpenses(head, month);
+    if (expenses != -1) {
+        printf("Expenses for %s: $%.2f\n", month, expenses);
+    }
+    else {
+        printf("No data found for %s.\n", month);
+    }
+}
+
+
+int main(void)
 {
-    
+
     struct Node* head = NULL;
 
-    
+
     char months[12][20] = { "January", "February", "March", "April", "May", "June",
                            "July", "August", "September", "October", "November", "December" };
 
     float dummy_expenses = 1000.0;
 
-    for (int i = 0; i < 12; i++) 
+    for (int i = 0; i < 12; i++)
     {
         struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
         strcpy(new_node->month, months[i]);
-        new_node->expenses = dummy_expenses * (i + 1); 
+        new_node->expenses = dummy_expenses * (i + 1);
         new_node->next = NULL;
         new_node->prev = NULL;
 
-        if (head == NULL) 
+        if (head == NULL)
         {
             head = new_node;
         }
-        else 
+        else
         {
             struct Node* current = head;
             while (current->next != NULL)
@@ -101,24 +136,49 @@ int main(void)
         }
     }
 
-    
+
     char input_month[20];
     printf("Enter a month to search for: ");
     scanf("%s", input_month);
 
-   
+
     float expenses = searchMonthExpenses(head, input_month);
 
-    if (expenses != -1) 
+    if (expenses != -1)
     {
         printf("Expenses for %s: $%.2f\n", input_month, expenses);
     }
-    else 
+    else
     {
         printf("Month of %s is not found.\n", input_month);
     }
 
-   
+    int choice;
+    do {
+        displayMainMenu();
+        scanf("%d", &choice);
+        switch (choice) {
+        case 1:
+            displayOverallExpenses(head);
+            break;
+        case 2:
+            displayMonthlyExpenses(head);
+            break;
+        case 3:
+            // Add functionality to add a new expense
+            break;
+        case 4:
+            // Add functionality to delete an expense
+            break;
+        case 5:
+            printf("Exiting the program.\n");
+            break;
+        default:
+            printf("Invalid option. Please try again.\n");
+            break;
+        }
+    } while (choice != 5);
+
 
     return 0;
 }
